5f0386dd3a546d8beb767b5cb03f0ea1d96bfec4
yoshelinn <gabrielayoshelin@gmail.com>
2023-09-20 15:35:20 +0700

    Temporary push takut hilang

--- main/urls.py
+++ main/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('xml/', show_xml, name='show_xml'), 
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
-path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
 ]
