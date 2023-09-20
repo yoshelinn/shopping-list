45ff88ddfa990d2493481325c0eef3ed356fcd24
yoshelinn <gabrielayoshelin@gmail.com>
2023-09-20 15:26:18 +0700

    Push agar tidak hilang

--- main/views.py
+++ main/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponseRedirect
 from main.forms import ProductForm
 from django.urls import reverse
 from main.forms import Product
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     products = Product.objects.all()
@@ -24,3 +26,19 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
